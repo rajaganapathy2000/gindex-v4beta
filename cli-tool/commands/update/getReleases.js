03b30d3bcce54c1054e1c3634348a85d701d654e
Raja Ganapathy <prajaganapathy771@gmail.com>
2021-03-28 04:23:52 +0530

    Uploaded

---
+++ cli-tool/commands/update/getReleases.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const backendRegex = /^(backend).+/;
+
+module.exports = () => {
+return axios.get('https://api.github.com/repos/rajaganapathy2000/gindex-v4/releases').then(resp => {    const result = resp.data.filter(releases =>{
+      return backendRegex.test(releases.tag_name);
+    })[0]
+    return result
+  })
+}
